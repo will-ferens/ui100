179ede6436b84f807f7889bd0afd1685769c4674
_dream <will.ferens@gmail.com>
2020-05-16 11:33:50 -0600

    initialized repo, two days down

---
+++ 001/src/app.js
@@ -0,0 +1,12 @@
+const monthsSelector = document.querySelector('#months');
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const dropdownBuilder = () => {
+
+};
+const dropdownOptions = () => {
+
+}
+const showDropdown = () => {
+
+}
