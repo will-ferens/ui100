2256ae93e832fa2b11aed1ae7f2ebe68f1840c46
_dream <will.ferens@gmail.com>
2020-05-18 17:16:27 -0600

    frameworked 003, added dropdown js to 001

--- 001/src/app.js
+++ 001/src/app.js
@@ -1,12 +1,45 @@
-const monthsSelector = document.querySelector('#months');
-const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+const monthsList = document.querySelector('#monthsDropdown');
+const daysList = document.querySelector('#daysDropdown');
+const yearsList = document.querySelector('#yearsDropdown');
 
-const dropdownBuilder = () => {
+const months = [
+    {number: 1, name: 'January'}, 
+    {number: 2, name: 'February'}, 
+    {number: 3, name: 'March'}, 
+    {number: 4, name: 'April'}, 
+    {number: 5, name: 'May'}, 
+    {number: 6, name: 'June'}, 
+    {number: 7, name: 'July'}, 
+    {number: 8, name: 'August'}, 
+    {number: 9, name: 'September'}, 
+    {number: 10, name: 'October'}, 
+    {number: 11, name: 'November'}, 
+    {number: 12, name: 'December'}
+];
 
+const dropdownBuilder = (element, data) => {
+    data.forEach(item => {
+        let li = document.createElement('li');
+        
+        li.classList.add('dropdown-item');
+        li.innerHTML = item.name;
+        
+        element.appendChild(li);
+    });
 };
+
 const dropdownOptions = () => {
 
-}
-const showDropdown = () => {
+};
 
-}
+const showDropdown = (element) => {
+    let dropdownContainer = document.querySelector(element);
+
+    if(dropdownContainer.classList.contains('show')) {
+        dropdownContainer.classList.remove('show');
+    } else {
+        dropdownContainer.classList.add('show');
+    }
+};
+
+dropdownBuilder(monthsList, months);
