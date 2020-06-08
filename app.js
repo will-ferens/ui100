200ac991cc7b5582a5f1f36325279c3c18a0ec57
_dream <will.ferens@gmail.com>
2020-06-07 18:52:25 -0600

    added simple server

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const path = require('path');
+
+const directoryPath = path.join(__dirname, 'public');
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+fs.readdir(directoryPath, function (err, files) {
+    //handling error
+    if (err) {
+        return console.log('Unable to scan directory: ' + err);
+    } 
+    //listing all files using forEach
+    files.forEach(function (file) {
+        // Do whatever you want to do with the file
+        app.get('/', function(req, res) { 
+
+            res.sendFile(file + '/index.html'); 
+        })
+    });
+});
+
+app.listen(8080, function() {
+    console.log('howdy')
+});
